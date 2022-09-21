9e8adf46f98773fc8f0ffb3c874e447744613e44
Julian <julian@julianovski.de>
2022-09-21 12:11:35 +0200

    Update scripts.js
    Added pokemonList and filled the array!

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
-alert('Hello world!');
-let favoriteFood = "Lasagna";
-document.write(favoriteFood);
+// For Task 1.1, I just continued with the program!
+// alert('Hello world!');
+// let favoriteFood = "Lasagna";
+// document.write(favoriteFood);
+
+let pokemonList = [];
+let bulbasaur = {
+    id: 1,
+    name: 'Bulbasaur',
+    type: ['grass', 'poison'],
+    height: 0.7,
+    weight: 6.9
+};
+let ivysaur = {
+    id: 2,
+    name: 'Ivysaur',
+    type: ['grass', 'poison'],
+    height: 1,
+    weight: 13
+};
+let venusaur = {
+    id: 3,
+    name: 'Venusaur',
+    type: ['grass', 'poison'],
+    height: 2,
+    weight: 100
+};
+
+pokemonList = [bulbasaur, ivysaur, venusaur]
